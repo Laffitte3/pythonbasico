aa4063a3bcd1e61a857e6dca1c92bbe2440a8eaf
Teresa Figuera <alejandromelrina324@gmail.com>
2024-02-17 18:17:02 -0400

    pythonbasico

---
+++ tabla_multiplicar_for.py
@@ -0,0 +1,40 @@
+
+
+"""num1=int(input("Ingresa un numero: "))
+
+result=0
+i=0
+
+for i in range(0,11):
+
+    if(i>=0):
+        result=num1*i
+        
+        """"""print(str(num1)+"*"+str(i)+"="+str(result)) """"""
+        print(f" {num1} * {i} = {result}")
+    if(i<0):
+        print("Solo acepto numeros positivos")"""
+
+"""weekday= input("Day: ")
+
+if (weekday == ("Domingo"or "domingo"or "DOMINGO")):
+    
+    print("ES IGUALLLL")
+else:
+    print("No funciona")"""
+
+
+nombre=[4, 1, 9, 7]
+
+
+"""for i in range(6):
+    print(nombre[2])
+    print(nombre[0])
+"""
+
+"""START:STOP:STEP
+print(nombre[0:3:2])"""
+
+for i in nombre:
+    if(i==7):
+        print(f"hola{i}")
